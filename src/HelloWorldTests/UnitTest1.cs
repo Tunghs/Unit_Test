314d669e5e99722c0d69a2119968fb9fdb7ee943
Tunghs <juk08843@gmail.com>
2022-03-17 22:14:54 +0900

    * 프로젝트생성

---
+++ src/HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace HelloWorldTests
+{
+    public class Tests
+    {
+        private const string Expected = "Hello World!";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorld.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
